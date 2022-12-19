d8cf8d218abb18d126653605d2295a3443918b70
Derek Gustafson <degustaf@gmail.com>
2022-12-19 11:15:10 -0500

    Remove docs directory.

--- docs/assets/bogfolk.js
+++
@@ -1,8 +0,0 @@
-function navbarOnclick() {
-  const navbar = document.getElementById('navbar');
-  if (navbar.classList.contains('mobile-navbar-display')) {
-    navbar.classList.remove('mobile-navbar-display');
-  } else {
-    navbar.classList.add('mobile-navbar-display');
-  }
-}
